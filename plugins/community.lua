18e4e6a4dd4a90b0ea2ecb605cb3af1ed4208398
crott <crott@pop-os.localdomain>
2023-11-15 12:31:22 -0500

    adding catpuc to colorschem

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,26 +1,13 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.colorscheme.nightfox-nvim", enabled = false },
+  { import = "astrocommunity.colorscheme.nightfox-nvim", enabled = true },
   { import = "astrocommunity.colorscheme.kanagawa-nvim", enabled = true },
   { import = "astrocommunity.colorscheme.rose-pine" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { -- further customize the options set by the community
-    "catppuccin",
-    opts = {
-      integrations = {
-        sandwich = false,
-        noice = true,
-        mini = true,
-        leap = true,
-        markdown = true,
-        neotest = true,
-        cmp = true,
-        overseer = true,
-        lsp_trouble = true,
-        rainbow_delimiters = true,
-      },
-    },
+    "catppuccin/nvim",
+    name = "catppuccin",
   },
   { import = "astrocommunity.completion.copilot-lua" },
   { -- further customize the options set by the community
