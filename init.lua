8aeede72fbef74e6387ed452aa649953854454a7
Cole Rottenberg <cole.rottenberg@gmail.com>
2023-12-01 13:24:19 -0500

    changed to frappe

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "catppuccin-latte",
+  colorscheme = "catppuccin-frappe",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
