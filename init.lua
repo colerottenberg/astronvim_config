3fc4e89b5407750200c3a90999bcd836056435b8
crott <crott@pop-os.localdomain>
2023-12-03 20:47:01 -0500

    changed back to mocha

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "dracula",
+  colorscheme = "catppuccin-mocha",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
